16100005aee7c2510f8b230cfea30cd3c8f71b64
Nic Jackson <jackson.nic@gmail.com>
2022-12-03 00:33:38 +0000

    working functions

--- functions.go
+++ functions.go
@@ -13,82 +13,183 @@ import (
 
 func createCtyFunctionFromGoFunc(f interface{}) (function.Function, error) {
 	// get the parameters
-	params := []function.Parameter{}
+	inParams := []function.Parameter{}
+	var outParam function.TypeFunc
+
+	var outType reflect.Kind
+	inType := []reflect.Kind{}
 
 	rf := reflect.TypeOf(f)
+
+	if rf.NumOut() != 2 {
+		return function.Function{}, fmt.Errorf("HCL functions must return two parameters, the result and an error i.e func(a,b int) (int, error)")
+	}
+
 	for i := 0; i < rf.NumIn(); i++ {
 		fp := rf.In(i)
 
 		switch fp.Kind() {
 		case reflect.String:
-			params = append(params, function.Parameter{
-				Name:             fp.Name(),
-				Type:             cty.String,
-				AllowDynamicType: true,
-			})
+			appendParms(&inType, &inParams, fp.Name(), reflect.String, cty.String)
 		case reflect.Int16:
-			fallthrough
+			appendParms(&inType, &inParams, fp.Name(), reflect.Int16, cty.Number)
 		case reflect.Int32:
-			fallthrough
+			appendParms(&inType, &inParams, fp.Name(), reflect.Int32, cty.Number)
 		case reflect.Int64:
-			fallthrough
+			appendParms(&inType, &inParams, fp.Name(), reflect.Int64, cty.Number)
 		case reflect.Int:
-			params = append(params, function.Parameter{
-				Name:             fp.Name(),
-				Type:             cty.Number,
-				AllowDynamicType: true,
-			})
+			appendParms(&inType, &inParams, fp.Name(), reflect.Int, cty.Number)
+		case reflect.Uint:
+			appendParms(&inType, &inParams, fp.Name(), reflect.Uint, cty.Number)
+		case reflect.Uint16:
+			appendParms(&inType, &inParams, fp.Name(), reflect.Uint16, cty.Number)
+		case reflect.Uint32:
+			appendParms(&inType, &inParams, fp.Name(), reflect.Uint32, cty.Number)
+		case reflect.Uint64:
+			appendParms(&inType, &inParams, fp.Name(), reflect.Uint64, cty.Number)
+		case reflect.Float32:
+			appendParms(&inType, &inParams, fp.Name(), reflect.Float32, cty.Number)
+		case reflect.Float64:
+			appendParms(&inType, &inParams, fp.Name(), reflect.Float64, cty.Number)
 		default:
 			return function.Function{}, fmt.Errorf("type %v is not a valid cyt type, only primative types like string and basic numbers are supported", fp.Kind())
 		}
 	}
 
-	var outType function.TypeFunc
-	outParam := rf.Out(0)
-	switch outParam.Kind() {
+	outType = rf.Out(0).Kind()
+	switch outType {
 	case reflect.String:
-		outType = function.StaticReturnType(cty.String)
+		outParam = function.StaticReturnType(cty.String)
 	case reflect.Int16:
 		fallthrough
 	case reflect.Int32:
 		fallthrough
 	case reflect.Int64:
 		fallthrough
+	case reflect.Uint16:
+		fallthrough
+	case reflect.Uint32:
+		fallthrough
+	case reflect.Uint64:
+		fallthrough
+	case reflect.Float32:
+		fallthrough
+	case reflect.Float64:
+		fallthrough
 	case reflect.Int:
-		outType = function.StaticReturnType(cty.Number)
+		fallthrough
+	case reflect.Uint:
+		outParam = function.StaticReturnType(cty.Number)
 	default:
 		return function.Function{}, fmt.Errorf("type %v is not a valid cyt type, only primative types like string and basic numbers are supported", rf.Out(0).Kind())
 	}
 
 	return function.New(&function.Spec{
-		Params: params,
+		Params: inParams,
 		Impl: func(args []cty.Value, retType cty.Type) (cty.Value, error) {
 
 			// create the params
 			in := []reflect.Value{}
-			for _, a := range args {
+			for i, a := range args {
 				switch a.Type() {
 				case cty.String:
 					in = append(in, reflect.ValueOf(a.AsString()))
 				case cty.Number:
 					bf := a.AsBigFloat()
-					val, _ := bf.Int64()
-					in = append(in, reflect.ValueOf(int(val)))
+					switch inType[i] {
+					case reflect.Int16:
+						val, _ := bf.Int64()
+						in = append(in, reflect.ValueOf(int16(val)))
+					case reflect.Int32:
+						val, _ := bf.Int64()
+						in = append(in, reflect.ValueOf(int32(val)))
+					case reflect.Int64:
+						val, _ := bf.Int64()
+						in = append(in, reflect.ValueOf(int64(val)))
+					case reflect.Int:
+						val, _ := bf.Int64()
+						in = append(in, reflect.ValueOf(int(val)))
+					case reflect.Uint16:
+						val, _ := bf.Uint64()
+						in = append(in, reflect.ValueOf(uint16(val)))
+					case reflect.Uint32:
+						val, _ := bf.Uint64()
+						in = append(in, reflect.ValueOf(uint32(val)))
+					case reflect.Uint64:
+						val, _ := bf.Uint64()
+						in = append(in, reflect.ValueOf(uint64(val)))
+					case reflect.Uint:
+						val, _ := bf.Uint64()
+						in = append(in, reflect.ValueOf(uint(val)))
+					case reflect.Float32:
+						val, _ := bf.Float64()
+						in = append(in, reflect.ValueOf(float32(val)))
+					case reflect.Float64:
+						val, _ := bf.Float64()
+						in = append(in, reflect.ValueOf(float64(val)))
+					}
 				}
 			}
 
 			out := reflect.ValueOf(f).Call(in)
 
 			switch retType {
+			case cty.String:
+				if out[1].Interface() == nil {
+					return cty.StringVal(out[0].String()), nil
+				} else {
+					return cty.StringVal(out[0].String()), out[1].Interface().(error)
+				}
 			case cty.Number:
-				return cty.NumberIntVal(out[0].Int()), nil
-
+				switch outType {
+				case reflect.Int16:
+					fallthrough
+				case reflect.Int32:
+					fallthrough
+				case reflect.Int64:
+					fallthrough
+				case reflect.Int:
+					if out[1].Interface() == nil {
+						return cty.NumberIntVal(out[0].Int()), nil
+					} else {
+						return cty.NumberIntVal(out[0].Int()), out[1].Interface().(error)
+					}
+				case reflect.Uint16:
+					fallthrough
+				case reflect.Uint32:
+					fallthrough
+				case reflect.Uint64:
+					fallthrough
+				case reflect.Uint:
+					if out[1].Interface() == nil {
+						return cty.NumberUIntVal(out[0].Uint()), nil
+					} else {
+						return cty.NumberUIntVal(out[0].Uint()), out[1].Interface().(error)
+					}
+				case reflect.Float32:
+					fallthrough
+				case reflect.Float64:
+					if out[1].Interface() == nil {
+						return cty.NumberFloatVal(out[0].Float()), nil
+					} else {
+						return cty.NumberFloatVal(out[0].Float()), out[1].Interface().(error)
+					}
+				}
 			}
 
 			return cty.NullVal(retType), nil
 		},
-		Type: outType,
+		Type: outParam,
 	}), nil
+}
+
+func appendParms(inType *[]reflect.Kind, params *[]function.Parameter, name string, kind reflect.Kind, typ cty.Type) {
+	*inType = append(*inType, kind)
+	*params = append(*params, function.Parameter{
+		Name:             name,
+		Type:             typ,
+		AllowDynamicType: true,
+	})
 }
 
 func getDefaultFunctions(filePath string) map[string]function.Function {
--- functions_test.go
+++ functions_test.go
@@ -51,11 +51,18 @@ func TestCreateFunctionWithInvalidOutParameterReturnsError(t *testing.T) {
 
 	_, err := createCtyFunctionFromGoFunc(myfunc)
 	require.Error(t, err)
+
+	myfunc2 := func(a string, b int) int {
+		return 1
+	}
+
+	_, err = createCtyFunctionFromGoFunc(myfunc2)
+	require.Error(t, err)
 }
 
 func TestCreateFunctionCallsFunction(t *testing.T) {
-	myfunc := func(a, b int) int {
-		return a + b
+	myfunc := func(a, b int) (int, error) {
+		return a + b, nil
 	}
 
 	ctyFunc, err := createCtyFunctionFromGoFunc(myfunc)
@@ -66,5 +73,40 @@ func TestCreateFunctionCallsFunction(t *testing.T) {
 
 	bf := val.AsBigFloat()
 	i, _ := bf.Int64()
-	require.Equal(t, 5, i)
+	require.Equal(t, int64(5), i)
+}
+
+func TestCreateFunctionHandlesInputParams(t *testing.T) {
+	type testCase struct {
+		name string
+		f    interface{}
+	}
+
+	cases := []testCase{
+		{
+			name: "integer input parameters",
+			f: func(a, b int) (int, error) {
+				return a + b, nil
+			},
+		},
+		{
+			name: "int16 input parameters",
+			f: func(a, b int16) (int16, error) {
+				return a + b, nil
+			},
+		},
+		{
+			name: "int32 input parameters",
+			f: func(a, b int32) (int32, error) {
+				return a + b, nil
+			},
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			_, err := createCtyFunctionFromGoFunc(c.f)
+			require.NoError(t, err)
+		})
+	}
 }
