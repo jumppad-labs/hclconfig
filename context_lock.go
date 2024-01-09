bad16592abb2c82b60a611a56affa919807096e1
Nic Jackson <jackson.nic@gmail.com>
2024-01-09 08:49:25 +0000

    Ensure context lock manages race conditions

--- context_lock.go
+++ context_lock.go
@@ -11,17 +11,7 @@ var locks = sync.Map{}
 // getContextLock ensures that a HCL Context is not written and read
 // at the same time
 func getContextLock(ctx *hcl.EvalContext) func() {
-	var lock any
-	var ok bool
-
-	lock, ok = locks.Load(ctx)
-
-	// lazy instantiate the lock
-	if !ok {
-		lock = &sync.Mutex{}
-
-		locks.Store(ctx, lock)
-	}
+	lock, _ := locks.LoadOrStore(ctx, &sync.Mutex{})
 
 	// obtain a lock
 	lock.(*sync.Mutex).Lock()
