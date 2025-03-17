45b8825ce5f5a6871ee714e9bf449b96d71d97f7
Nic Jackson <jackson.nic@gmail.com>
2025-03-17 21:05:20 +0000

    Add depth to serialize, add struct tests

--- schema/serialize_test.go
+++ schema/serialize_test.go
@@ -1,92 +1,119 @@
 package schema
 
 import (
+	"fmt"
 	"testing"
 
 	fixtures "github.com/jumppad-labs/hclconfig/schema/test_fixtures"
 	"github.com/stretchr/testify/require"
 )
 
+const maxDepth = 10
+
 func TestSerializeInt(t *testing.T) {
-	b, err := GenerateFromInstance(fixtures.MyInt{})
+	b, err := GenerateFromInstance(fixtures.MyInt{}, maxDepth)
 	require.NoError(t, err)
 
 	require.JSONEq(t, fixtures.MyIntJSON, string(b))
 }
 
 func TestSerializeIntSlice(t *testing.T) {
-	b, err := GenerateFromInstance(fixtures.MyIntSlice{})
+	b, err := GenerateFromInstance(fixtures.MyIntSlice{}, maxDepth)
 	require.NoError(t, err)
 
 	require.JSONEq(t, fixtures.MyIntSliceJSON, string(b))
 }
 
 func TestSerializeIntPtr(t *testing.T) {
-	b, err := GenerateFromInstance(fixtures.MyIntPtr{})
+	b, err := GenerateFromInstance(fixtures.MyIntPtr{}, maxDepth)
 	require.NoError(t, err)
 
 	require.JSONEq(t, fixtures.MyIntPtrJSON, string(b))
 }
 
 func TestSerializeIntPtrSlice(t *testing.T) {
-	b, err := GenerateFromInstance(fixtures.MyIntPtrSlice{})
+	b, err := GenerateFromInstance(fixtures.MyIntPtrSlice{}, maxDepth)
 	require.NoError(t, err)
 
 	require.JSONEq(t, fixtures.MyIntPtrSliceJSON, string(b))
 }
 
 func TestSerializeString(t *testing.T) {
-	b, err := GenerateFromInstance(fixtures.MyString{})
+	b, err := GenerateFromInstance(fixtures.MyString{}, maxDepth)
 	require.NoError(t, err)
 
 	require.JSONEq(t, fixtures.MyStringJSON, string(b))
 }
 
 func TestSerializeStringSlice(t *testing.T) {
-	b, err := GenerateFromInstance(fixtures.MyStringSlice{})
+	b, err := GenerateFromInstance(fixtures.MyStringSlice{}, maxDepth)
 	require.NoError(t, err)
 
 	require.JSONEq(t, fixtures.MyStringSliceJSON, string(b))
 }
 
 func TestSerializeStringPtr(t *testing.T) {
-	b, err := GenerateFromInstance(fixtures.MyStringPtr{})
+	b, err := GenerateFromInstance(fixtures.MyStringPtr{}, maxDepth)
 	require.NoError(t, err)
 
 	require.JSONEq(t, fixtures.MyStringPtrJSON, string(b))
 }
 
 func TestSerializeStringPtrSlice(t *testing.T) {
-	b, err := GenerateFromInstance(fixtures.MyStringPtrSlice{})
+	b, err := GenerateFromInstance(fixtures.MyStringPtrSlice{}, maxDepth)
 	require.NoError(t, err)
 
 	require.JSONEq(t, fixtures.MyStringPtrSliceJSON, string(b))
 }
 
 func TestSerializeBool(t *testing.T) {
-	b, err := GenerateFromInstance(fixtures.MyBool{})
+	b, err := GenerateFromInstance(fixtures.MyBool{}, maxDepth)
 	require.NoError(t, err)
 
 	require.JSONEq(t, fixtures.MyBoolJSON, string(b))
 }
 
 func TestSerializeBoolSlice(t *testing.T) {
-	b, err := GenerateFromInstance(fixtures.MyBoolSlice{})
+	b, err := GenerateFromInstance(fixtures.MyBoolSlice{}, maxDepth)
 	require.NoError(t, err)
 
 	require.JSONEq(t, fixtures.MyBoolSliceJSON, string(b))
 }
 
 func TestSerializeBoolPtr(t *testing.T) {
-	b, err := GenerateFromInstance(fixtures.MyBoolPtr{})
+	b, err := GenerateFromInstance(fixtures.MyBoolPtr{}, maxDepth)
 	require.NoError(t, err)
 
 	require.JSONEq(t, fixtures.MyBoolPtrJSON, string(b))
 }
 
 func TestSerializeBoolPtrSlice(t *testing.T) {
-	b, err := GenerateFromInstance(fixtures.MyBoolPtrSlice{})
+	b, err := GenerateFromInstance(fixtures.MyBoolPtrSlice{}, maxDepth)
 	require.NoError(t, err)
 
 	require.JSONEq(t, fixtures.MyBoolPtrSliceJSON, string(b))
+}
+
+func TestSerializeStructPtrDepth1(t *testing.T) {
+	b, err := GenerateFromInstance(fixtures.MyStructPtr{}, 1)
+	require.NoError(t, err)
+	fmt.Println(string(b))
+
+	require.JSONEq(t, fixtures.MyStructPtrDepth1JSON, string(b))
+}
+
+func TestSerializeStructPtrDepth2(t *testing.T) {
+	b, err := GenerateFromInstance(fixtures.MyStructPtr{}, 2)
+	require.NoError(t, err)
+	fmt.Println(string(b))
+
+	require.JSONEq(t, fixtures.MyStructPtrDepth2JSON, string(b))
+}
+
+func TestSerializeStructPtrDepth3(t *testing.T) {
+	b, err := GenerateFromInstance(fixtures.MyStructPtr{}, 3)
+	require.NoError(t, err)
+	fmt.Println(string(b))
+
+	require.JSONEq(t, fixtures.MyStructPtrDepth3JSON, string(b))
 }
---
+++ schema/test_fixtures/struct.go
@@ -0,0 +1,75 @@
+package fixtures
+
+type MyStructPtr struct {
+	Name   string       `json:"name"`
+	Struct *MyStructPtr `json:"struct"`
+}
+
+var MyStructPtrDepth1JSON = `{
+  "type": "fixtures.MyStructPtr",
+  "properties": [
+   {
+    "name": "Name",
+    "type": "string",
+    "tags": "json:\"name\""
+   }
+  ]
+ }`
+
+ var MyStructPtrDepth2JSON = `{
+  "type": "fixtures.MyStructPtr",
+  "properties": [
+   {
+    "name": "Name",
+    "type": "string",
+    "tags": "json:\"name\""
+   },
+   {
+    "name": "Struct",
+    "type": "*fixtures.MyStructPtr",
+    "tags": "json:\"struct\"",
+    "properties": [
+     {
+      "name": "Name",
+      "type": "string",
+      "tags": "json:\"name\""
+     }
+    ]
+   }
+  ]
+ }`
+
+ var MyStructPtrDepth3JSON = `{
+  "type": "fixtures.MyStructPtr",
+  "properties": [
+   {
+    "name": "Name",
+    "type": "string",
+    "tags": "json:\"name\""
+   },
+   {
+    "name": "Struct",
+    "type": "*fixtures.MyStructPtr",
+    "tags": "json:\"struct\"",
+    "properties": [
+     {
+      "name": "Name",
+      "type": "string",
+      "tags": "json:\"name\""
+     },
+     {
+      "name": "Struct",
+      "type": "*fixtures.MyStructPtr",
+      "tags": "json:\"struct\"",
+      "properties": [
+       {
+        "name": "Name",
+        "type": "string",
+        "tags": "json:\"name\""
+       }
+      ]
+     }
+    ]
+   }
+  ]
+ }`
